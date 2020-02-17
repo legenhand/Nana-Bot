3212bf4221f256bc07a208e008df690d172d3983
legenhand <firman@sagita.tech>
2020-02-17 17:18:51 +0700

    Final: Added Support gdrive_credentials for heroku

--- nana/assistant/gdrive.py
+++ nana/assistant/gdrive.py
@@ -15,7 +15,7 @@ async def gdrive_helper(client, message):
 		gdriveclient = os.path.isfile("client_secrets.json")
 		if HEROKU_API:
 			if not gdrive_credentials:
-				await message.reply("Hello, look like you're not logged in to google drive 🙂\nI can help you to login.\n\nFirst of all, you need to activate your google drive API\n1. [Go here](https://developers.google.com/drive/api/v3/quickstart/python), click **Enable the drive API**\n2. Login to your google account (skip this if you're already logged in)\n3. After logged in, click **Enable the drive API** again, and click **Download Client Configuration** button, download that.\n4. After downloaded that file, open that file then copy all of that content, back to telegram then do .credentials (copy the content of that file)  do without bracket \n\nAfter that, you can go next guide by type /gdrive")
+				await message.reply("Hello, look like you're not logged in to google drive 🙂\nI can help you to login.\n\nFirst of all, you need to activate your google drive API\n1. [Go here](https://developers.google.com/drive/api/v3/quickstart/python), click **Enable the drive API**\n2. Login to your google account (skip this if you're already logged in)\n3. After logged in, click **Enable the drive API** again, and click **Download Client Configuration** button, download that.\n4. After downloaded that file, open that file then copy all of that content\n\n go to dashboard.heroku.com, select your apps and go to settings, go to config vars then add key with name `gdrive_credentials` with value your credentials\n\nAfter that, you can go next guide by type /gdrive")
 				return
 			elif not gdriveclient:
 				file = open("client_secrets.json","w")

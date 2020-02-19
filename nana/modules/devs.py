3889537400907b602e49d4d618906dbfb095d860
legenhand <firman@sagita.tech>
2020-02-20 02:05:12 +0700

    Feature: Added speedtest, Usage speedtest

--- nana/modules/devs.py
+++ nana/modules/devs.py
@@ -7,7 +7,9 @@ import shutil
 import subprocess
 import sys
 import traceback
+import cv2
 
+from speedtest import Speedtest
 from platform import python_version, uname
 from nana import app, Command, logging, app, setbot, Owner, AdminSettings, DB_AVAIABLE, USERBOT_VERSION, ASSISTANT_VERSION, BotUsername
 from nana.helpers.deldog import deldog
@@ -41,6 +43,10 @@ Get user specific data center
 ──「 **Get Repo Nana-Bot** 」──
 -> `repo`
 Get Repo For this userbot
+
+──「 **Test Your Server Internet Speed** 」──
+-> `speedtest`
+Obtain Server internet speed using speedtest
 
 """
 
@@ -207,3 +213,37 @@ async def alive(client, message):
 		text += "\nBot logged in as `{}`\n Go to your assistant for more information!".format(me.first_name)
 	await message.edit(text)
 
+@app.on_message(Filters.user("self") & Filters.command(["speedtest"], Command))
+async def speedtest(client, message):
+	await message.edit("`Running speed test . . .`")
+	test = Speedtest()
+	test.get_best_server()
+	test.download()
+	test.upload()
+	test.results.share()
+	result = test.results.dict()
+	await message.edit("`"
+                   "Started at "
+                   f"{result['timestamp']} \n\n"
+                   "Download "
+                   f"{speed_convert(result['download'])} \n"
+                   "Upload "
+                   f"{speed_convert(result['upload'])} \n"
+                   "Ping "
+                   f"{result['ping']} \n"
+                   "ISP "
+                   f"{result['client']['isp']}"
+                   "`")
+
+
+def speed_convert(size):
+    """
+    Hi human, you can't read bytes?
+    """
+    power = 2**10
+    zero = 0
+    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"

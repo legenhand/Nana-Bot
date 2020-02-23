61019b12c22651d5d04c97b7635da7a5b1772bc9
legenhand <firman@sagita.tech>
2020-02-23 13:53:10 +0700

    Feature : Added mega module, usage mega (url)

--- nana/modules/mega.py
+++ nana/modules/mega.py
@@ -1,4 +1,4 @@
-from os import listdir
+import os
 
 from mega import Mega
 from pyrogram import Filters
@@ -13,6 +13,8 @@ async def mega_download(client, message):
         await message.edit("usage: mega (url)")
         return
     await message.edit("Processing...")
+    if not os.path.exists('nana/downloads/mega'):
+        os.makedirs('nana/downloads/mega')
     m = Mega()
     try:
         m.download_url(args[1], "nana/downloads/mega")
@@ -23,7 +25,7 @@ async def mega_download(client, message):
 
 @app.on_message(Filters.user("self") & Filters.command(["megafile"], Command))
 async def mega_downloaded_file(client, message):
-    filelist = listdir("nana/downloads/mega")
+    filelist = os.listdir("nana/downloads/mega")
     print(len(filelist))
     if len(filelist) == 0:
         await message.edit("You haven't download any files with mega! try to download something")

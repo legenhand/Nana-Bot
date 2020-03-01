b8ce7829a0e47cef1ef36d4ff2532c26f3f5237c
legenhand <firman@sagita.tech>
2020-03-01 21:29:13 +0700

    Feature : (experimental) Added lydia support for PM, it will activate to all message, usage 'lydiapv'

---
+++ nana/modules/lydia.py
@@ -0,0 +1,50 @@
+import time
+
+from coffeehouse import InvalidApiKeyError
+from coffeehouse.api import API
+from coffeehouse.lydia import LydiaAI
+from pyrogram import Filters
+
+from nana import lydia_api, app, Command
+
+lydia_status = False
+coffeehouse_api = None
+lydia = None
+session = None
+
+
+@app.on_message(Filters.user("self") & Filters.command(["lydiapv"], Command))
+async def lydia_private(client, message):
+    global lydia_status, coffeehouse_api, lydia, session
+    if lydia_api == "":
+        await message.edit("`lydia API key is not set!\nSet your lydia API key by adding Config Vars in heroku with "
+                           "name lydia_api with value your lydia key API`")
+        return
+    if lydia_status:
+        await message.edit("Turning off lydia...")
+        time.sleep(0.5)
+        lydia_status = False
+        await message.edit("Lydia will not reply your message")
+    else:
+        await message.edit("Turning on lydia...")
+        try:
+            coffeehouse_api = API(lydia_api)
+            # Create Lydia instance
+            lydia = LydiaAI(coffeehouse_api)
+            # Create a new chat session (Like a conversation)
+            session = lydia.create_session()
+        except InvalidApiKeyError as e:
+            await message.edit("Wrong lydia API key!")
+            return
+        lydia_status = True
+        await message.edit("now Lydia will reply your message!")
+
+
+@app.on_message(Filters.incoming & Filters.private)
+async def lydia_reply(client, message):
+    global lydia_status, session
+    if lydia_status:
+        output = session.think_thought(message.text)
+        await message.reply("`{0}`".format(output))
+    else:
+        return

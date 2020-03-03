c3d1b7522a4164d9cc616620d8db148faf5b09f0
legenhand <firman@sagita.tech>
2020-03-04 00:39:25 +0700

    Fix : Better urbandict formatting

--- nana/modules/urbandict.py
+++ nana/modules/urbandict.py
@@ -1,9 +1,9 @@
-import requests
 import json
 
+import requests
+from pyrogram import Filters
 
 from nana import app, Command
-from pyrogram import Filters
 
 __MODULE__ = "Urban Dictionary"
 __HELP__ = """
@@ -14,20 +14,31 @@ Search for urban dictionary
 Search urban for dictionary
 """
 
+
 @app.on_message(Filters.user("self") & Filters.command(["ud"], Command))
 async def urban_dictionary(client, message):
-	if len(message.text.split()) == 1:
-		await message.edit("Usage: `ud example`")
-		return
-	text = message.text.split(None, 1)[1]
-	response = requests.get("http://api.urbandictionary.com/v0/define?term={}".format(text))
-	if response.status_code == 200:
-		data = response.json()
-		word = json.dumps(data['list'][0]['word'])
-		definition = json.dumps(data['list'][0]['definition'])
-		example = json.dumps(data['list'][0]['example'])
-		teks = "**Result of {}**\n\n**{}**\n**Meaning:**\n`{}`\n\n**Example:**\n`{}`".format(text,word,definition.replace("[","").replace("]","").replace("\"","").replace("\\",""),example.replace("[","").replace("]","").replace("\"","").replace("\\","") )
-		await message.edit(teks)
-		return
-	elif response.status_code == 404:
-		await message.edit("Cannot connect to Urban Dictionary")
+    if len(message.text.split()) == 1:
+        await message.edit("Usage: `ud example`")
+        return
+    text = message.text.split(None, 1)[1]
+    response = requests.get("http://api.urbandictionary.com/v0/define?term={}".format(text))
+    if response.status_code == 200:
+        data = response.json()
+        word = json.dumps(data['list'][0]['word'])
+        definition = json.dumps(data['list'][0]['definition'])
+        example = json.dumps(data['list'][0]['example'])
+        teks = "**Result of {}**\n\n**{}**\n**Meaning:**\n{}\n\n**Example:**\n{}".format(text, word,
+                                                                                         replace_definition(definition),
+                                                                                         replace_example(example))
+        await message.edit(teks)
+        return
+    elif response.status_code == 404:
+        await message.edit("Cannot connect to Urban Dictionary")
+
+
+def replace_definition(text):
+    return text.replace("[", "<i><b>").replace("]", "</i></b>").replace("\"", "")
+
+
+def replace_example(text):
+    return replace_definition(text).replace("\\r", "").replace("\\n", "\n").replace("\\", "__")

5e896ac3accfbb8ebde08f30363b5020a2c15734
legenhand <firman@sagita.tech>
2020-03-08 20:12:41 +0700

    Fix : Update config.example.py

--- nana/config.example.py
+++ nana/config.example.py
@@ -1,17 +1,16 @@
 # Buat file config.py baru dalam dir dan impor yang sama, kemudian perpanjang kelas ini.
 class Config(object):
-	LOGGER = True
-	
+	LOGGER = False
 	# Must be filled!
 	# Register here: https://my.telegram.org/apps
 	api_id = 12345
 	api_hash = "123456789abcdefghijklmnopqrstuvw"
-	DATABASE_URL = "postgres://username:password@localhost:5432/database" # Your database URL
+	DATABASE_URL = "postgres://username:password@localhost:5432/database"  # Your database URL
 
 	# Version
-	lang_code = "en" # Your language code
-	device_model = "PC" # Device model
-	system_version = "Linux" # OS system type
+	lang_code = "en"  # Your language code
+	device_model = "PC"  # Device model
+	system_version = "Linux"  # OS system type
 
 	# Use real bot for Assistant
 	# Pass False if you dont want
@@ -19,39 +18,41 @@ class Config(object):
 	ASSISTANT_BOT_TOKEN = ""
 
 	# Required for some features
-	AdminSettings = [] # Insert int id, Add someone so they can access your assistant, leave it blank if you dont want!
-	Command = ["!", "."] # Insert command prefix, if you insert "!" then you can do !ping
-    # WORKER must be int (number)
-    NANA_WORKER = 8
-    ASSISTANT_WORKER = 2
-    # If True, send notification to user if Official branch has new update after running bot
-    REMINDER_UPDATE = True
+	AdminSettings = [
+		1231231]  # Insert int id, Add someone so they can access your assistant, leave it blank if you dont want!
+	Command = ["!", "."]  # Insert command prefix, if you insert "!" then you can do !ping
+	# WORKER must be int (number)
+	NANA_WORKER = 8
+	ASSISTANT_WORKER = 2
+	# If True, send notification to user if Official branch has new update after running bot
+	REMINDER_UPDATE = True
 
-    # APIs token
-    thumbnail_API = ""  # Register free here: https://thumbnail.ws/
-    screenshotlayer_API = ""  # Register free here: https://screenshotlayer.com/
-    bitly_token = ""
-    lydia_api = ""
-    HEROKU_API = ""
-    # Load or no load plugins
-    # userbot
-    USERBOT_LOAD = []
-    USERBOT_NOLOAD = []
-    # manager bot
-    ASSISTANT_LOAD = []
-    ASSISTANT_NOLOAD = []
+	# APIs token
+	thumbnail_API = ""  # Register free here: https://thumbnail.ws/
+	screenshotlayer_API = ""  # Register free here: https://screenshotlayer.com/
+	bitly_token = ""  # register here : bitly.com
+	lydia_api = ""  # register here : https://coffeehouse.intellivoid.info/
+	HEROKU_API = ""  # if you're using heroku this field must filled, get from here : https://dashboard.heroku.com/account
+	# Load or no load plugins
+	# userbot
+	USERBOT_LOAD = []
+	USERBOT_NOLOAD = []
+	# manager bot
+	ASSISTANT_LOAD = []
+	ASSISTANT_NOLOAD = []
 
-    # Fill this if you want to login using session code, else leave it blank
+	# Fill this if you want to login using session code, else leave it blank
 	USERBOT_SESSION = ""
 	ASSISTANT_SESSION = ""
 
 	# Pass True if you want to use test mode
 	TEST_MODE = False
-	
+
 
 class Production(Config):
 	LOGGER = False
 
 
 class Development(Config):
-	LOGGER = True
+	TEST_DEVELOP = None
+	LOGGER = False
--- nana/modules/purges.py
+++ nana/modules/purges.py
@@ -1,10 +1,7 @@
-import os
-import pyrogram
 import math
+from datetime import datetime
 
-from nana import app, Owner, Command
-
-from pyrogram import Filters
+from nana import Owner, setbot
 
 __MODULE__ = "Purges"
 __HELP__ = """
@@ -45,73 +42,124 @@ Give a number **without reply** to purge for x messages.
 Purge your messages only, no need admin permission.
 """
 
-#@app.on_message(Filters.user("self") & Filters.command(["purge"], Command))
+
+# @app.on_message(Filters.user("self") & Filters.command(["purge"], Command))
 async def purge(client, message):
-	if message.reply_to_message:
-		is_reply = True
-		target = message.reply_to_message.message_id
-	elif len(message.text.split()) >= 2 and message.text.split()[1].isdigit():
-		is_reply = False
-		target = int(message.text.split()[1])
-	else:
-		await message.edit("Reply to the message to purge until that, or give me a number!")
-	if not is_reply:
-		get_msg = await client.get_history(message.chat.id, limit=target+1)
-		listall = [x.message_id for x in get_msg]
-	else:
-		dari = message.message_id + 1
-		listall = [x for x in range(target, dari)]
-		listall.reverse()
-	if len(listall) >= 101:
-		total = len(listall)
-		semua = listall
-		jarak = 0
-		jarak2 = 0
-		for x in range(math.ceil(len(listall)/100)):
-			if total >= 101:
-				jarak2 += 100
-				await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
-				jarak += 100
-				total -= 100
-			else:
-				jarak2 += total
-				await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
-				jarak += total
-				total -= total
-	else:
-		await client.delete_messages(message.chat.id, message_ids=listall)
+    if message.reply_to_message:
+        is_reply = True
+        target = message.reply_to_message.message_id
+    elif len(message.text.split()) >= 2 and message.text.split()[1].isdigit():
+        is_reply = False
+        target = int(message.text.split()[1])
+    else:
+        await message.edit("Reply to the message to purge until that, or give me a number!")
+    if not is_reply:
+        get_msg = await client.get_history(message.chat.id, limit=target + 1)
+        listall = [x.message_id for x in get_msg]
+    else:
+        dari = message.message_id + 1
+        listall = [x for x in range(target, dari)]
+        listall.reverse()
+    if len(listall) >= 101:
+        total = len(listall)
+        semua = listall
+        jarak = 0
+        jarak2 = 0
+        for x in range(math.ceil(len(listall) / 100)):
+            if total >= 101:
+                jarak2 += 100
+                await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
+                jarak += 100
+                total -= 100
+            else:
+                jarak2 += total
+                await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
+                jarak += total
+                total -= total
+    else:
+        await client.delete_messages(message.chat.id, message_ids=listall)
 
 
-#@app.on_message(Filters.user("self") & Filters.command(["purgeme"], Command))
+# @app.on_message(Filters.user("self") & Filters.command(["purgeme"], Command))
 async def purge_myself(client, message):
-	if len(message.text.split()) >= 2 and message.text.split()[1].isdigit():
-		target = int(message.text.split()[1])
-	else:
-		await message.edit("Give me a number for a range!")
-	get_msg = await client.get_history(message.chat.id)
-	listall = []
-	counter = 0
-	for x in get_msg:
-		if counter == target+1:
-			break
-		if x.from_user.id == int(Owner):
-			listall.append(x.message_id)
-			counter += 1
-	if len(listall) >= 101:
-		total = len(listall)
-		semua = listall
-		jarak = 0
-		jarak2 = 0
-		for x in range(math.ceil(len(listall)/100)):
-			if total >= 101:
-				jarak2 += 100
-				await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
-				jarak += 100
-				total -= 100
-			else:
-				jarak2 += total
-				await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
-				jarak += total
-				total -= total
-	else:
-		await client.delete_messages(message.chat.id, message_ids=listall)
+    if len(message.text.split()) >= 2 and message.text.split()[1].isdigit():
+        target = int(message.text.split()[1])
+    else:
+        await message.edit("Give me a number for a range!")
+    get_msg = await client.get_history(message.chat.id)
+    listall = []
+    counter = 0
+    for x in get_msg:
+        if counter == target + 1:
+            break
+        if x.from_user.id == int(Owner):
+            listall.append(x.message_id)
+            counter += 1
+    if len(listall) >= 101:
+        total = len(listall)
+        semua = listall
+        jarak = 0
+        jarak2 = 0
+        for x in range(math.ceil(len(listall) / 100)):
+            if total >= 101:
+                jarak2 += 100
+                await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
+                jarak += 100
+                total -= 100
+            else:
+                jarak2 += total
+                await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
+                jarak += total
+                total -= total
+    else:
+        await client.delete_messages(message.chat.id, message_ids=listall)
+
+
+# @app.on_message(Filters.user("self") & Filters.command(["purge"], Command))
+async def purgetest(client, message):
+    if message.reply_to_message:
+        start_t = datetime.now()
+        recvd_commands = message.text.split(" ")
+        from_user = None
+        if len(recvd_commands) > 1:
+            user_id = recvd_commands[1]
+            from_user = await client.get_users(user_id)
+        start_message = message.reply_to_message.message_id
+        end_message = message.message_id
+        list_of_messages = await client.get_messages(
+            chat_id=message.chat.id,
+            message_ids=range(start_message, end_message),
+            replies=0
+        )
+        # print(list_of_messages)
+        list_of_messages_to_delete = []
+        purged_messages_count = 0
+        for a_message in list_of_messages:
+            if len(list_of_messages_to_delete) == 100:
+                await client.delete_messages(
+                    chat_id=message.chat.id,
+                    message_ids=list_of_messages_to_delete,
+                    revoke=True
+                )
+                purged_messages_count += len(list_of_messages_to_delete)
+                list_of_messages_to_delete = []
+            if from_user is not None:
+                if a_message.from_user == from_user:
+                    list_of_messages_to_delete.append(a_message.message_id)
+            else:
+                list_of_messages_to_delete.append(a_message.message_id)
+        print(list_of_messages_to_delete)
+        await client.delete_messages(
+            chat_id=message.chat.id,
+            message_ids=list_of_messages_to_delete,
+            revoke=True
+        )
+        purged_messages_count += len(list_of_messages_to_delete)
+        list_of_messages_to_delete = []
+        end_t = datetime.now()
+        time_taken_s = (end_t - start_t).seconds
+        await setbot.send_message(Owner,
+                                  f"`purged {purged_messages_count} messages in {time_taken_s} seconds.`"
+                                  )
+    else:
+        await message.edit("Reply to a message to purge [user's] messages.")

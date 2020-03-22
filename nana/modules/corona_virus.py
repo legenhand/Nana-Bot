1a97bf060304fba13121cc9f0c5dcf73e738081d
legenhand <firman@sagita.tech>
2020-03-22 22:52:15 +0700

    Fix : Added exception if country not found

--- nana/modules/corona_virus.py
+++ nana/modules/corona_virus.py
@@ -17,21 +17,30 @@ Check info of cases corona virus disease 2019
 @app.on_message(Filters.user("self") & Filters.command(["corona"], Command))
 async def corona(client, message):
     args = message.text.split(None, 1)
+    if len(args) == 1:
+        await message.edit("`usage : corona (country)`")
+        return
     covid = Covid()
     data = covid.get_data()
     country = args[1]
     country_data = get_country_data(country.capitalize(), data)
-    output_text = "`Confirmed : {}\n`".format(country_data["confirmed"])
-    output_text += "`Active : {}`\n".format(country_data["active"])
-    output_text += "`Deaths : {}`\n".format(country_data["deaths"])
-    output_text += "`Recovered : {}`\n".format(country_data["recovered"])
-    output_text += "`Last update : {}`\n". \
-        format(datetime.utcfromtimestamp(country_data["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
+    if country_data:
+        output_text = "`Confirmed   : {}\n`".format(country_data["confirmed"])
+        output_text += "`Active      : {}`\n".format(country_data["active"])
+        output_text += "`Deaths      : {}`\n".format(country_data["deaths"])
+        output_text += "`Recovered   : {}`\n".format(country_data["recovered"])
+        output_text += "`Last update : {}`\n". \
+            format(datetime.utcfromtimestamp(country_data["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
+        output_text += "`Data provided by `<a href=\"https://j.mp/2xf6oxF\">Johns Hopkins University</a>"
+    else:
+        output_text = "`No information yet about this country!`"
     await message.edit("**Corona Virus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
+    # TODO : send location of country
+    # await client.send_location(message.chat.id, float(country_data["latitude"]), float(country_data["longitude"]))
 
 
 def get_country_data(country, world):
     for country_data in world:
         if country_data["country"] == country:
             return country_data
-    return {"Status": "No information yet about this country!"}
+    return

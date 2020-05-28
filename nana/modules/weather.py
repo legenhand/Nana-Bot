1d51f5e024a5931f37c2fc388ca826ad04ed0bc0
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-28 17:08:04 +0500

    Update weather.py
    Weather ASCII Art Fixed

--- nana/modules/weather.py
+++ nana/modules/weather.py
@@ -21,14 +21,17 @@ Powered by `wttr.in`
 @app.on_message(Filters.user("self") & Filters.command(["weather"], Command))
 async def weather(client, message):
     if len(message.text.split()) == 1:
-        await message.edit("Usage: `weather jakarta`")
+        await message.edit("Usage: `weather Maldives`")
         return
     location = message.text.split(None, 1)[1]
     h = {'user-agent': 'httpie'}
     a = requests.get(f"https://wttr.in/{location}?mnTC0&lang={lang_code}", headers=h)
-
     if "Sorry, we processed more than 1M requests today and we ran out of our datasource capacity." in a.text:
         await message.edit("Sorry, location not found!")
         return
 
-    await message.edit(f"<code>{escape(a.text)}</code>", parse_mode='html')
+    we = f"<code>{escape(a.text)}</code>"
+    weather ="`{}`\n".format(we)
+
+
+    await message.edit(weather, parse_mode='html')

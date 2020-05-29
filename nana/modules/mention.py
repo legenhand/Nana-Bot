5751832e4741bc661191aa42ec9537e80ee796e9
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-29 06:20:59 +0500

    Create mention.py
    Mention a user with custom text

---
+++ nana/modules/mention.py
@@ -0,0 +1,26 @@
+from pyrogram import Filters
+from nana import Command, app
+
+__MODULE__ = "Mention"
+__HELP__ = """
+──「 **Mention** 」──
+-> `mention (username without @) (custom text)`
+Generate a  hyperlink username you refer with a custom single text.
+
+"""
+
+@app.on_message(Filters.user("self") & Filters.command([".mention"], Command))
+async def mention(client, message):
+    args = message.text.split(None, 2)
+    if len(args) == 3:
+        user = args[1]
+        name = args[2]
+        rep = f'<a href="tg://resolve?domain={user}">{name}</a>'
+        await message.edit(
+            rep,
+            disable_web_page_preview=True,
+            parse_mode="html"
+        )
+    else:
+        await message.edit("Usage: `.mention (username without @) (custom text)`")
+        return

98f12884a8d6e2d06de56e23dbb3592cecac84b1
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-30 11:17:25 +0500

    Update nekobin.py
    Issues with nekobin fixed

--- nana/modules/nekobin.py
+++ nana/modules/nekobin.py
@@ -14,31 +14,20 @@ Create a Nekobin paste using replied to message.
 
 @app.on_message(Filters.user("self") & Filters.command(["neko"], Command))
 async def paste(client, message):
-    if len(message.text.split()) == 1:
-        await message.edit("Usage: `neko (text) or neko (reply to a text)`")
-        return
-    await message.edit_text("`Pasting...`")
-    if message.reply_to_message.message_id:
-        splitter = message.text.split(None, 1)
-        if len(splitter) == 1:
-            text = message.reply_to_message.text or message.reply_to_message.caption
+    if message.reply_to_message:
+        await message.edit_text("`Pasting...`")
+        text = message.reply_to_message.text
+        try:
+            key = requests.post('https://nekobin.com/api/documents', json={"content": text}).json().get('result').get('key')
+        except requests.exceptions.RequestException:
+            await message.edit_text("`Pasting failed`")
+            await asyncio.sleep(2)
+            await message.delete()
         else:
-            text = splitter[1]
+            reply_text = f'`Successfully pasted on` [Nekobin](https://nekobin.com/{key})'
+            await message.edit_text(
+                reply_text,
+                disable_web_page_preview=True,
+            )
     else:
-        splitter = message.text.split(None, 1)
-        if len(splitter) == 1:
-            return
-        else:
-            text = splitter[1]
-    try:
-        key = requests.post('https://nekobin.com/api/documents', json={"content": text}).json().get('result').get('key')
-    except requests.exceptions.RequestException as e:
-        await message.edit_text("`Pasting failed`")
-        await asyncio.sleep(2)
-        await message.delete()
-    else:
-        reply_text = f'`Successfully pasted on` [Nekobin](https://nekobin.com/{key})'
-        await message.edit_text(
-            reply_text,
-            disable_web_page_preview=True,
-        )
+        await message.edit_text("Usage: `neko (reply to a text)`")

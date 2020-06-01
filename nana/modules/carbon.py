ec3bd1b94669a8f201b915107b69bce5a0f24ee4
AzaroTaro <amanhmedzahir@hotmail.com>
2020-06-01 17:18:13 +0500

    Revert "Update carbon.py"
    This reverts commit 084781626046ad4b4bee5aefb0ddd6bf1bad0e32.

--- nana/modules/carbon.py
+++ nana/modules/carbon.py
@@ -1,68 +1,37 @@
-# Based on https://github.com/cyberboysumanjay/Carbon-API
-# Author of Carbon API: Sumanjay (https://github.com/cyberboysumanjay) (@cyberboysumanjay)
-# All rights reserved.
-
-import requests
+from requests import post
+import shutil
+import os
+from nana import Command, app
 from pyrogram import Filters
-from nana import app, Command
+from time import sleep
+from nana.helpers.PyroHelpers import ReplyCheck
 
-__MODULE__ = "Carbon API"
-__HELP__ = """
-Create Beautiful Snippets of your code!
-
-──「 **Carbon** 」──
--> `carbon (text or reply to msg | theme-name | colour code)`
-
-Example: carbon print('Hello World') | lucario | #48e8e3
-Themes Supported:
-`3024-night`, `a11y-dark`, `blackboard`, `base16-dark`, `base16-light`,
-`cobalt`, `dracula`, `duotone-dark`, `hopscotch`, `lucario`, `material`,
-`monokai`, `night-owl`, `nord`, `oceanic-next`, `one-light`, `one-dark`,
-`panda-syntax`, `paraiso-dark`, `seti`, `shades-of-purple`, `solarized-dark`,
-`solarized-light`, `synthwave-84`, `twilight`, `verminal`, `vscode`,`yeti`, `zenburn`"
-"""
-
-theme = 'dracula'
-bg = "rgba(0, 255, 230, 100)"
-themes = [
-    '3024-night', 'a11y-dark', 'blackboard', 'base16-dark', 'base16-light',
-    'cobalt', 'dracula', 'duotone-dark', 'hopscotch', 'lucario', 'material',
-    'monokai', 'night-owl', 'nord', 'oceanic-next', 'one-light', 'one-dark',
-    'panda-syntax', 'paraiso-dark', 'seti', 'shades-of-purple', 'solarized-dark',
-    'solarized-light', 'synthwave-84', 'twilight', 'verminal', 'vscode',
-    'yeti', 'zenburn']
+CARBON_LANG = "Auto"
 
 @app.on_message(Filters.user("self") & Filters.command(["carbon"], Command))
-async def carbon(client, message):
-    replied = message.reply_to_message
-    if replied:
-        text = replied.text
-        args = message.text.split('|')
+async def carbon_api(client, message):
+    json = {
+        "backgroundColor": "rgba(0, 255, 230, 100)",
+        "theme": "Dracula",
+        "exportSize": "4x"
+    }
+    if message.reply_to_message:
+        r = message.reply_to_message
+        json["code"] = r.text
+        await message.edit_text("Carbonizing code...")
     else:
-        text = message.text
-        args = text.split('|')
-
-    if len(args) > 0:
-        for arg in args:
-            arg = arg.strip()
-            if arg.lower().replace(" ", "-") in themes:
-                theme = arg.lower()
-            elif arg.startswith("#") or arg.startswith("rgb"):
-                if arg[0] == "#" and len(arg) == 7:
-                    arg = arg[1:]
-                bg = arg
-            elif len(arg) > 0:
-                text = arg
-
-    if not text:
-        await message.err("Code not found!")
-        return
-    await message.edit("Carbonizing the Code")
-    try:
-        carbon_result = requests.get(
-            "https://sjprojectsapi.herokuapp.com/carbon/?"
-            f"text={text}&theme={theme}&bg={bg}").json()
-        await client.send_photo(chat_id=message.chat.id, photo=carbon_result['link'])
+        await message.edit("Usage: `carbon` (reply to a code or text)")
+    json["language"] = CARBON_LANG
+    apiUrl = "http://carbonnowsh.herokuapp.com"
+    r = post(apiUrl,json=json,stream=True)
+    filename = 'carbon.png'
+    if r.status_code == 200:
+        r.raw.decode_content = True
+        with open(filename,'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        await client.send_document(message.chat.id, filename, reply_to_message_id=ReplyCheck(message))
         await message.delete()
-    except Exception:
-        await message.edit("API is Down! Try again later.")
+    else:
+        await message.edit('Image Couldn\'t be retreived')
+        await message.delete()
+    os.remove(filename)

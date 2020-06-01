31ef4c8d63934cf4136016e865309ac3e4e66013
AzaroTaro <amanhmedzahir@hotmail.com>
2020-06-01 19:00:30 +0500

    Update meme.py
    Spam

--- nana/modules/meme.py
+++ nana/modules/meme.py
@@ -77,6 +77,22 @@ async def mocking_text(text):
         pesan += teks[x]
     return pesan
 
+@app.on_message(Filters.user("self") & Filters.command(["spam"], Command))
+async def spam(client, message):
+    await message.delete()
+    times = message.command[1]
+    to_spam = ' '.join(message.command[2:])
+
+    if message.chat.type in ['supergroup', 'group']:
+        for _ in range(int(times)):
+            await client.send_message(message.chat.id, to_spam, reply_to_message_id=ReplyCheck(message))
+            await asyncio.sleep(0.20)
+
+    if message.chat.type == "private":
+        for _ in range(int(times)):
+            await client.send_message(message.chat.id, to_spam)
+            await asyncio.sleep(0.20)
+
 @app.on_message(Filters.user("self") & Filters.command(["waifu"], Command))
 async def waifu(client, message):
     await message.delete()

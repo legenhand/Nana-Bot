96f9966e1d15524563609b69682ba758a9a8b162
AzaroTaro <amanhmedzahir@hotmail.com>
2020-06-03 04:04:21 +0500

    Update cas.py
    issue with replycheck will fix later lel

--- nana/modules/cas.py
+++ nana/modules/cas.py
@@ -12,7 +12,8 @@ from nana.helpers.PyroHelpers import ReplyCheck
 __MODULE__ = "CAS Scanner"
 __HELP__ = """
 ──「 **Combot Anti Spam Check** 」──
--> `cas` - userid
+-> `cas` @username
+-> `cas` (reply to a text) To find information about a person.
 
 """
 
@@ -23,15 +24,21 @@ def replace_text(text):
 @app.on_message(Filters.me & Filters.command(["cas"], Command))
 async def cas(client, message):
     cmd = message.command
-
-    user = ""
-    if len(cmd) > 1:
-        user = " ".join(cmd[1:])
+    if not message.reply_to_message and len(cmd) == 1:
+        get_user = message.from_user.id
     elif message.reply_to_message and len(cmd) == 1:
-        user = message.reply_to_message.text
-    elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("`Usage: cas user-id`")
-        await asyncio.sleep(2)
+        get_user = message.reply_to_message.from_user.id
+    elif len(cmd) > 1:
+        get_user = cmd[1]
+        try:
+            get_user = int(cmd[1])
+        except ValueError:
+            pass
+    try:
+        user = await client.get_users(get_user)
+    except PeerIdInvalid:
+        await message.edit("I don't know that User.")
+        sleep(2)
         await message.delete()
         return
     results = requests.get(f'https://api.cas.chat/check?user_id={user}').json()
@@ -39,4 +46,4 @@ async def cas(client, message):
         reply_text = f'`User ID: `{user}\n`Offenses: `{results["result"]["offenses"]}\n`Messages: `\n{results["result"]["messages"]}\n`Time Added: `{results["result"]["time_added"]}'
     except:
         reply_text = "`Record not found.`"
-    await message.edit(replace_text(reply_text), reply_to_message_id=ReplyCheck(message))
+    await message.edit(replace_text(reply_text))

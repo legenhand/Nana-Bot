8dded87188d8cb485c6c4fe8cb94f8d168f0b0ab
AzaroTaro <amanhmedzahir@hotmail.com>
2020-06-05 16:52:27 +0500

    Update purge.py
    purge command fixed

--- nana/modules/purge.py
+++ nana/modules/purge.py
@@ -40,38 +40,38 @@ Purge your messages only, no need admin permission.
 @app.on_message(Filters.me & Filters.command(["purge"], Command))
 async def purge(client, message):
     if message.reply_to_message:
-        is_reply = True
-        target = message.reply_to_message.message_id
-    elif len(message.text.split()) >= 2 and message.text.split()[1].isdigit():
-        is_reply = False
-        target = int(message.text.split()[1])
-    else:
-        await message.edit("Reply to the message to purge until that, or give me a number!")
-    if not is_reply:
-        get_msg = await client.get_history(message.chat.id, limit=target + 1)
-        listall = [x.message_id for x in get_msg]
-    else:
-        dari = message.message_id + 1
-        listall = [x for x in range(target, dari)]
-        listall.reverse()
-    if len(listall) >= 101:
-        total = len(listall)
-        semua = listall
-        jarak = 0
-        jarak2 = 0
-        for x in range(math.ceil(len(listall) / 100)):
-            if total >= 101:
-                jarak2 += 100
-                await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
-                jarak += 100
-                total -= 100
+        start_t = datetime.now()
+        user_id = None
+        from_user = None
+        start_message = message.reply_to_message.message_id
+        end_message = message.message_id
+        list_of_messages = await client.get_messages(chat_id=message.chat.id,
+                                                    message_ids=range(start_message, end_message),
+                                                    replies=0)
+        list_of_messages_to_delete = []
+        purged_messages_count = 0
+        for a_message in list_of_messages:
+            if len(list_of_messages_to_delete) == 100:
+                await client.delete_messages(chat_id=message.chat.id,
+                                            message_ids=list_of_messages_to_delete,
+                                            revoke=True)
+                purged_messages_count += len(list_of_messages_to_delete)
+                list_of_messages_to_delete = []
+            if from_user is not None:
+                if a_message.from_user == from_user:
+                    list_of_messages_to_delete.append(a_message.message_id)
             else:
-                jarak2 += total
-                await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
-                jarak += total
-                total -= total
+                list_of_messages_to_delete.append(a_message.message_id)
+        await client.delete_messages(chat_id=message.chat.id,
+                                    message_ids=list_of_messages_to_delete,
+                                    revoke=True)
+        purged_messages_count += len(list_of_messages_to_delete)
+        end_t = datetime.now()
+        time_taken_s = (end_t - start_t).seconds
+        await message.delete()
     else:
-        await client.delete_messages(message.chat.id, message_ids=listall)
+        out = "Reply to a message to to start purge."
+        await message.delete()
 
 
 @app.on_message(Filters.me & Filters.command(["purgeme"], Command))

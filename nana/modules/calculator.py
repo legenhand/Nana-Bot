ad3481ca5264942e7d9b171be3c0508b178e57cf
Poki <aman@virtual.mv>
2020-06-13 19:27:22 +0500

    [PYL-E0102] - Redefined functions replaced with new functions

--- nana/modules/calculator.py
+++ nana/modules/calculator.py
@@ -90,7 +90,7 @@ async def evaluation_curr(_client, message):
 
 
 @app.on_message(Filters.me & Filters.command(["temp"], Command))
-async def evaluation_curr(_client, message):
+async def evaluation_temp(_client, message):
     if len(message.text.split()) <= 2:
         await message.edit("Usage: `temp 30 C` or `temp 60 F`")
         return

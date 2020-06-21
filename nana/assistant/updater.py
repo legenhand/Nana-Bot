1b14f4d1ffd41ed6ec8d3732e8db69ba10456239
Poki <aman@virtual.mv>
2020-06-21 22:21:47 +0500

    fixed time import

--- nana/assistant/updater.py
+++ nana/assistant/updater.py
@@ -1,9 +1,9 @@
 import random
+import time
 
 from git import Repo
 from git.exc import GitCommandError, NoSuchPathError, InvalidGitRepositoryError
 from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton
-
 from nana import setbot, Owner, USERBOT_VERSION, ASSISTANT_VERSION, log, OFFICIAL_BRANCH, \
     REPOSITORY, RANDOM_STICKERS, REMINDER_UPDATE, TEST_DEVELOP, HEROKU_API, DB_AVAILABLE
 from nana.__main__ import restart_all, loop

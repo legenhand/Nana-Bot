6fa5e7838bc825d5147a54099925a02d25d2f50b
Poki <aman@virtual.mv>
2020-06-23 06:21:12 +0500

    fixed voice

--- nana/modules/voice.py
+++ nana/modules/voice.py
@@ -43,11 +43,10 @@ async def voice(client, message):
     if len(message.text.split()) == 1:
         await message.edit("Send text then change to audio")
         return
-    await message.delete()
     await client.send_chat_action(message.chat.id, "record_audio")
-    text = message.text.split(None, 1)[1]
     tts = gTTS(text, lang=lang)
     tts.save('nana/cache/voice.mp3')
+    await message.delete()
     if message.reply_to_message:
         await client.send_voice(message.chat.id, voice="nana/cache/voice.mp3",
                                 reply_to_message_id=message.reply_to_message.message_id)

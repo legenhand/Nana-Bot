2ee962a85acbde586160cd75d009474bbdc1cb68
Poki <aman@virtual.mv>
2020-06-24 06:00:31 +0500

    Torch for Termux Added [Experimental]
    Idk if this gonna work or not but dont update nana remix yet

--- nana/modules/termux.py
+++ nana/modules/termux.py
@@ -16,7 +16,12 @@ And install Termux API from Playstore.
 -> `bstats`
 get yout phone's Battery Status.
 
+──「 **Torch** 」──
+-> `torch`
+Turn your Device's Torch on & off.
+
 """
+torch = False
 
 @app.on_message(Filters.me & Filters.command(["bstats"], Command))
 async def bstat(_client, message):
@@ -29,3 +34,27 @@ async def bstat(_client, message):
         await message.edit("This command is only for Termux users!")
         await sleep(2.0)
         await message.delete()
+
+
+@app.on_message(Filters.me & Filters.command(["torch"], Command))
+async def termux_torch(_client, message):
+    global torch
+    if torch:
+        await message.edit("Turning off torch...")
+        await sleep(0.5)
+        subprocess.Popen("termux-torch off", shell=True, stdout=subprocess.PIPE)
+        torch = False
+        await message.edit("Torch turned off")
+        await sleep(2.0)
+        await message.delete()
+    else:
+        await message.edit("Turning on torch...")
+        try:
+            subprocess.Popen("termux-torch on", shell=True, stdout=subprocess.PIPE)
+        except:
+            await message.edit("Couldn't turn off torch!")
+            await sleep(2.0)
+            await message.delete()
+            return
+        torch = True
+        await message.edit("Torch turned on!")

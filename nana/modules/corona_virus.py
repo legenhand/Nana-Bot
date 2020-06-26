34312e9e1a0e98f569971c4af9ff475753c9c636
Poki <aman@virtual.mv>
2020-06-27 04:14:22 +0500

    much cleaner covid with aiohttp

--- nana/modules/corona_virus.py
+++ nana/modules/corona_virus.py
@@ -7,73 +7,43 @@ import requests
 from pyrogram import Filters
 
 from nana import Command, app
+from nana.helpers.aiohttp_helper import AioHttp
 
 __MODULE__ = "Covid Info"
 __HELP__ = """
 Check info of cases corona virus disease 2019
 
 ──「 **Info Covid** 」──
--> `corona - for Global Stats`
--> `corona (country) - for a Country Stats`
+-> `covid - for Global Stats`
+-> `covid (country) - for a Country Stats`
 """
 
 
-@app.on_message(Filters.me & Filters.command(["corona"], Command))
+@app.on_message(Filters.me & Filters.command(["covid"], Command))
 async def corona(_client, message):
-    await message.edit("`Processing...`")
     args = message.text.split(None, 1)
     if len(args) == 1:
-        r = requests.get("https://corona.lmao.ninja/v2/all?yesterday=true").json()
-        last_updated = datetime.datetime.fromtimestamp(r['updated'] / 1000).strftime("%Y-%m-%d %I:%M:%S")
-
-        ac = PrettyTable()
-        ac.header = False
-        ac.title = "Global Statistics"
-        ac.add_row(["Cases", f"{r['cases']:,}"])
-        ac.add_row(["Cases Today", f"{r['todayCases']:,}"])
-        ac.add_row(["Deaths", f"{r['deaths']:,}"])
-        ac.add_row(["Deaths Today", f"{r['todayDeaths']:,}"])
-        ac.add_row(["Recovered", f"{r['recovered']:,}"])
-        ac.add_row(["Active", f"{r['active']:,}"])
-        ac.add_row(["Critical", f"{r['critical']:,}"])
-        ac.add_row(["Cases/Million", f"{r['casesPerOneMillion']:,}"])
-        ac.add_row(["Deaths/Million", f"{r['deathsPerOneMillion']:,}"])
-        ac.add_row(["Tests", f"{r['tests']:,}"])
-        ac.add_row(["Tests/Million", f"{r['testsPerOneMillion']:,}"])
-        ac.align = "l"
-
-        await message.edit(f"```{str(ac)}```\nLast updated on: {last_updated}")
+        try:
+            r = await AioHttp().get_json(f"https://corona.lmao.ninja/v2/all")
+            reply_text = f"**Global Cases 🦠:**\nCases: `{r['cases']:,}`\nCases Today: `{r['todayCases']:,}`\nDeaths: `{r['deaths']:,}`\nDeaths Today: `{r['todayDeaths']:,}`\nRecovered: `{r['recovered']:,}`\nActive: `{r['active']:,}`\nCritical: `{r['critical']:,}`\nCases/Mil: `{r['casesPerOneMillion']}`\nDeaths/Mil: `{r['deathsPerOneMillion']}``"
+            await message.edit(f"{reply_text}")
+        except Exception as e:
+            await message.edit("`The corona API could not be reached`")
+            print(e)
+            await asyncio.sleep(3)
+            await message.delete()
     country = args[1]
-    r = requests.get(f"https://corona.lmao.ninja/v2/countries/{country}").json()
+    r = await AioHttp().get_json(f"https://corona.lmao.ninja/v2/countries/{country}")
     if "cases" not in r:
         await message.edit("```The country could not be found!```")
         await asyncio.sleep(3)
         await message.delete()
     else:
-        last_updated = datetime.datetime.fromtimestamp(r['updated'] / 1000).strftime("%Y-%m-%d %I:%M:%S")
-
-        cc = PrettyTable()
-        cc.header = False
-        country = r['countryInfo']['iso3'] if len(r['country']) > 12 else r['country']
-        cc.title = f"Corona Cases in {country}"
-        cc.add_row(["Cases", f"{r['cases']:,}"])
-        cc.add_row(["Cases Today", f"{r['todayCases']:,}"])
-        cc.add_row(["Deaths", f"{r['deaths']:,}"])
-        cc.add_row(["Deaths Today", f"{r['todayDeaths']:,}"])
-        cc.add_row(["Recovered", f"{r['recovered']:,}"])
-        cc.add_row(["Active", f"{r['active']:,}"])
-        cc.add_row(["Critical", f"{r['critical']:,}"])
-        cc.add_row(["Cases/Million", f"{r['casesPerOneMillion']:,}"])
-        cc.add_row(["Deaths/Million", f"{r['deathsPerOneMillion']:,}"])
-        cc.add_row(["Tests", f"{r['tests']:,}"])
-        cc.add_row(["Tests/Million", f"{r['testsPerOneMillion']:,}"])
-        cc.align = "l"
-        await message.edit(f"```{str(cc)}```\nLast updated on: {last_updated}")
-
-
-def get_country_data(country, world):
-    for country_data in world:
-        if country_data["country"] == country:
-            return country_data
-    return
-
+        try:
+            reply_text = f"**Cases for {r['country']} 🦠**\nCases: `{r['cases']:,}`\nCases Today: `{r['todayCases']:,}`\nDeaths: `{r['deaths']:,}`\nDeaths Today: `{r['todayDeaths']:,}`\nRecovered: `{r['recovered']:,}`\nActive: `{r['active']:,}`\nCritical: `{r['critical']:,}`\nCases/Mil: `{r['casesPerOneMillion']}`\nDeaths/Mil: `{r['deathsPerOneMillion']}``"
+            await message.edit(f"{reply_text}")
+        except Exception as e:
+            await message.edit("`The corona API could not be reached`")
+            print(e)
+            await asyncio.sleep(3)
+            await message.delete()

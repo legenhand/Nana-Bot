d5ec0b84cf26f71616c514ab12b42ca8928011d8
Poki <aman@virtual.mv>
2020-06-30 04:24:31 +0500

    video notes 0_0

--- nana/modules/downloads.py
+++ nana/modules/downloads.py
@@ -499,6 +499,33 @@ def time_formatter(milliseconds: int) -> str:
     return tmp[:-2]
 
 
+async def download_reply_nocall(client, message):
+	if message.reply_to_message.photo:
+		nama = "photo_{}_{}.png".format(message.reply_to_message.photo.file_id, message.reply_to_message.photo.date)
+		await client.download_media(message.reply_to_message.photo, file_name="nana/downloads/" + nama)
+	elif message.reply_to_message.animation:
+		nama = "giphy_{}-{}.gif".format(message.reply_to_message.animation.date, message.reply_to_message.animation.file_size)
+		await client.download_media(message.reply_to_message.animation, file_name="nana/downloads/" + nama)
+	elif message.reply_to_message.video:
+		nama = "video_{}-{}.mp4".format(message.reply_to_message.video.date, message.reply_to_message.video.file_size)
+		await client.download_media(message.reply_to_message.video, file_name="nana/downloads/" + nama)
+	elif message.reply_to_message.sticker:
+		nama = "sticker_{}_{}.webp".format(message.reply_to_message.sticker.date, message.reply_to_message.sticker.set_name)
+		await client.download_media(message.reply_to_message.sticker, file_name="nana/downloads/" + nama)
+	elif message.reply_to_message.audio:
+		nama = "{}".format(message.reply_to_message.audio.file_name)
+		await client.download_media(message.reply_to_message.audio, file_name="nana/downloads/" + nama)
+	elif message.reply_to_message.voice:
+		nama = "audio_{}.ogg".format(message.reply_to_message.voice)
+		await client.download_media(message.reply_to_message.voice, file_name="nana/downloads/" + nama)
+	elif message.reply_to_message.document:
+		nama = "{}".format(message.reply_to_message.document.file_name)
+		await client.download_media(message.reply_to_message.document, file_name="nana/downloads/" + nama)
+	else:
+		return False
+	return "nana/downloads/" + nama
+
+
 async def download_file_from_tg(client, message):
     await message.edit("__Downloading...__")
     start = int(time.time())

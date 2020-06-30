813d91781fffea33e52523e4eb2bb92dea58c822
pokurt <amanhmedzahir@hotmail.com>
2020-07-01 03:01:59 +0500

    pep8 fixes

--- nana/modules/quotly.py
+++ nana/modules/quotly.py
@@ -17,7 +17,7 @@ Reply To Message Text To Create Quote Sticker
 
 
 @app.on_message(Filters.me & Filters.command(["q"], Command))
-async def quotly(client, message):
+async def q_maker(_client, message):
     if not message.reply_to_message:
         await message.edit("Reply to any users text message")
         return
@@ -30,13 +30,14 @@ async def quotly(client, message):
             msg = await app.get_history("@QuotLyBot", 1)
             check = msg[0]["sticker"]["file_id"]
             is_sticker = True
-        except:
+        except Exception as e:
+            print(e)
             time.sleep(0.5)
             progress += random.randint(0, 10)
             try:
-                await message.edit("```Making a Quote```\nProcessing {}%".format(progress))
-            except:
-                await message.edit("ERROR SUUUU")
+                await message.edit(f"```Making a Quote```\nProcessing {progress}%")
+            except Exception as e:
+                await message.edit(f'**ERROR:**\n{e}')
     await message.edit("```Complete !```")
     msg_id = msg[0]["message_id"]
     await app.forward_messages(message.chat.id, "@QuotLyBot", msg_id)

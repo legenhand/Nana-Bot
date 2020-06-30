dc08034a3c4dae6a06c4ebf3422cf27e753c21ba
pokurt <amanhmedzahir@hotmail.com>
2020-07-01 03:01:17 +0500

    pep8 fixes

--- nana/helpers/deldog.py
+++ nana/helpers/deldog.py
@@ -2,10 +2,9 @@ import requests
 
 
 async def deldog(message, data):
-    BASE_URL = 'https://del.dog'
-    r = requests.post(f'{BASE_URL}/documents', data=data.encode('utf-8'))
+    r = requests.post('https://del.dog/documents', data=data.encode('utf-8'))
     if r.status_code == 404:
-        await message.edit('Failed to reach dogbin')
+        await message.edit('Failed to reach dog.del')
         r.raise_for_status()
     res = r.json()
     if r.status_code != 200:
@@ -13,7 +12,7 @@ async def deldog(message, data):
         r.raise_for_status()
     key = res['key']
     if res['isUrl']:
-        reply = f'Shortened URL: {BASE_URL}/{key}\nYou can view stats, etc. [here]({BASE_URL}/v/{key})'
+        reply = f'Shortened URL: https://del.dog/{key}\nView stats, etc. [here](https://del.dog/v/{key})'
     else:
-        reply = f'{BASE_URL}/{key}'
+        reply = f'https://del.dog/{key}'
     return reply

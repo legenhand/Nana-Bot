2e668be0035a601036b0c8b541237e54ece90071
pokurt <amanhmedzahir@hotmail.com>
2020-07-01 23:48:01 +0500

    Fixed replace ' ' with '_' for downloads

--- nana/modules/googledrive.py
+++ nana/modules/googledrive.py
@@ -141,14 +141,14 @@ async def gdrive_stuff(client, message):
             await message.edit(
                 "Invaild URL!\nIf you think this is bug, please go to your Assistant bot and type `/reportbug`")
             return
-        await message.edit("Downloading for `{}`\nPlease wait...".format(filename))
+        await message.edit("Downloading for `{}`\nPlease wait...".format(filename.replace(' ', '_')))
         download = drive.CreateFile({'id': driveid})
         download.GetContentFile(filename)
         try:
-            os.rename(filename, "nana/downloads/" + filename)
+            os.rename(filename, "nana/downloads/" + filename.replace(' ', '_'))
         except FileExistsError:
-            os.rename(filename, "nana/downloads/" + filename + ".2")
-        await message.edit("Downloaded!\nFile saved to `{}`".format("nana/downloads/" + filename))
+            os.rename(filename, "nana/downloads/" + filename.replace(' ', '_') + ".2")
+        await message.edit("Downloaded!\nFile saved to `{}`".format("nana/downloads/" + filename.replace(' ', '_')))
     elif len(message.text.split()) == 3 and message.text.split()[1] == "upload":
         filerealname = message.text.split()[2].split(None, 1)[0]
         filename = "nana/downloads/{}".format(filerealname.replace(' ', '_'))

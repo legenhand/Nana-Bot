dbfe3878f23280fe0df3be469e9ee31f0edf96f4
firman <firman@sagita.tech>
2020-08-30 19:52:46 +0700

    Fix : Nana broke on ENV mode

--- nana/startup/var.py
+++ nana/startup/var.py
@@ -1,11 +1,11 @@
 import os
-from nana.config import Development as Config
 
 def get_var(name, default=None):
     ENV = bool(os.environ.get('ENV', False))
     if ENV:
         return os.environ.get(name, default)
     else:
+        from nana.config import Development as Config
         try:
             return getattr(Config, name)
         except AttributeError:

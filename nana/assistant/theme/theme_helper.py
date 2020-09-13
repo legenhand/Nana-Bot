08c31a8ffd2ac595ae87b3e642c1b2064c9e9af2
firman <firman@sagita.tech>
2020-09-13 16:43:20 +0700

    Feature : Finishing Theme Changer Feature.
    TODO : Added User Theme Customization

--- nana/assistant/theme/theme_helper.py
+++ nana/assistant/theme/theme_helper.py
@@ -13,10 +13,16 @@ async def get_theme(name, type):
 async def theme_list():
     theme = await AioHttp().get_json('http://api.harumi.tech/theme')
     list_button = []
-    print(theme["Nana-Official"]["theme-code"])
-    for i in theme.items():
-        call_code = theme[i]["theme-code"]
-        print(i, call_code)
+    for i in theme:
+        theme_code = theme[i]["theme-code"]
+        call_code = f"thm-{theme_code}"
         list_button.append([InlineKeyboardButton(i, callback_data=call_code)])
     list_button.append([InlineKeyboardButton("⬅ back️", callback_data="back")])
     return InlineKeyboardMarkup(list_button)
+
+
+async def name_theme(theme_code):
+    theme = await AioHttp().get_json('http://api.harumi.tech/theme')
+    for i in theme:
+        if theme_code == theme[i]["theme-code"]:
+            return i
--- nana/assistant/theme_modules.py
+++ nana/assistant/theme_modules.py
@@ -1,11 +1,11 @@
 from pyrogram import Filters
-from nana import setbot
-from .theme.theme_helper import theme_list
+from nana import setbot, Owner
+from .database.theme_db import set_name_theme_set
+from .theme.theme_helper import theme_list, name_theme
 
 
 @setbot.on_callback_query(Filters.regex("theme"))
 async def chgtheme(_client, query):
-    print("test")
     text = "**⚙️Theme Configuration **\n" \
            "`Change Your Nana Theme Here! `\n"
 
@@ -13,3 +13,14 @@ async def chgtheme(_client, query):
     await query.message.edit_text(text, reply_markup=button)
 
 
+@setbot.on_callback_query(Filters.regex("^thm"))
+async def chgtheme(_client, query):
+    code_theme = query.data[4:]
+    name = await name_theme(code_theme)
+    await set_name_theme_set(Owner, name)
+    text = "**⚙️Theme Configuration **\n" \
+           f"Theme Changed To `{name}`\n"
+
+    await query.message.edit_text(text)
+
+# TODO : Added User Theme Customization
